4dfc519c72ceef46023f723b44feb09ee77de2a8
Soumya Ray <soumya.ray@gmail.com>
2018-10-09 19:07:45 +0800

    Database integrated into app

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require 'roda'
 require 'concurrent-ruby'
+require_relative 'db/database'
 
 class MyApplication < Roda
   #...
   # add some routing code here
   # add some documentation
   # integrate api libary
+  # integrate datbase
 end
